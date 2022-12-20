67464662d3ba2bfd309212fe532a5383c7511fa3
shaimamathlouthi <shaymamathlouthi@ieee.org>
2022-12-20 22:13:09 +0100

    work_player

--- lib/controller/player_controller.dart
+++ lib/controller/player_controller.dart
@@ -5,7 +5,10 @@ import '../model/player_model.dart';
 class PlayerController extends ChangeNotifier {
   List<Player> players = [];
 
-  void addPlayer() {}
+  void addPlayer(Player player) {
+    players.add(player);
+    notifyListeners();
+  }
 
   void deletePlayer() {}
 
